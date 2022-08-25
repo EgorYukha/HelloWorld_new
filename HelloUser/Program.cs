635d42d8f18c19d4e9e5a868cebc1f9168c1e29f
EgorYukha <yukha90@mail.ru>
2022-08-25 18:55:59 +0300

    add commit Hello User

---
+++ HelloUser/Program.cs
@@ -0,0 +1,9 @@
+﻿//console.WriteLine("Введите ваше имя");
+//String username = Console.readline ();
+//Console.WriteLine("Привет, ");
+//Console.WriteLine(username);
+// Console.WriteLine("2 Hello, World!");
+Console.WriteLine("Введите ваше имя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
